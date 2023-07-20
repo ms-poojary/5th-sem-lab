a0297d496a28c90211966dd56158ecc5e12bb134
azlanajju <muhammedazlan11@gmail.com>
2023-07-20 20:53:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function copyProgram(program, button) {
+  console.log("copied");
+  var copyText = document.getElementById(program);
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+
+  var copiedElement = document.getElementById(button);
+  copiedElement.innerHTML = "Copied!";
+
+  setTimeout(function() {
+    if (copiedElement.innerHTML === "Copied!") {
+      copiedElement.innerHTML = "";
+    }
+  }, 5000); 
+}
+
+function switchProgram(current_pgm) {
+  var containers = document.getElementsByClassName('textarea_container');
+  for (var i = 0; i < containers.length; i++) {
+      var container = containers[i];
+
+          container.style.display = "none";
+          document.getElementById(current_pgm).style.display="flex";
+
+  }
+}
+
+function closeProgram() {
+  var containers = document.getElementsByClassName('textarea_container');
+  for (var i = 0; i < containers.length; i++) {
+      var container = containers[i];
+
+          container.style.display = "none";
+
+  }
+}
