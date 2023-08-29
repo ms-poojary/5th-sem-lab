d55ead2ece4e64f4420bd37fd448b7007bc4786f
ms-poojary <varshinipoojary04@gmail.com>
2023-08-29 16:15:24 +0530

    python codes are colored

--- main.js
+++ main.js
@@ -16,24 +16,6 @@ function copyProgram(programId, copiedId) {
 }
 
 
-function copyProgramTextarea(program, button) {
-  console.log("copied");
-  var copyText = document.getElementById(program);
-  copyText.select(); 
-  navigator.clipboard.writeText(copyText.value);
-
-  var copiedElement = document.getElementById(button);
-  copiedElement.innerHTML = "Copied!";
-
-  setTimeout(function() {
-    if (copiedElement.innerHTML === "Copied!") {
-      copiedElement.innerHTML = "";
-    }
-  }, 5000); 
-}
-
-
-
 function switchProgram(current_pgm) {
   var containers = document.getElementsByClassName('textarea_container');
   for (var i = 0; i < containers.length; i++) {
@@ -41,7 +23,6 @@ function switchProgram(current_pgm) {
 
           container.style.display = "none";
           document.getElementById(current_pgm).style.display="flex";
-
   }
 }
 
@@ -52,14 +33,12 @@ function closeProgram() {
       var container = containers[i];
 
           container.style.display = "none";
-
   }
 }
 
 
 function openNav() {
   var navWidth = document.getElementById('pgmList');
-
   if (navWidth.style.width === "60vw") {
     navWidth.style.width = "0";
   } else {
